112bc182ed236e72addaaa28c80fecc964e335bc
Dinesh Perumal <2contactdinesh@gmail.com>
2025-10-02 10:51:13 +0530

    Fix Homebrew dylib linkage errors - v1.0.2
    - Added needs_linkage_fixing? method to skip automatic linkage fixing
    - This prevents the libEGL.dylib header space errors during Homebrew installation
    - Bumped version to 1.0.2 for the fix
    - Updated checksums for new release files

--- homebrew/k8s-file-transfer.rb
+++ homebrew/k8s-file-transfer.rb
@@ -1,24 +1,34 @@
 class K8sFileTransfer < Formula
   desc "GUI application for transferring files between local machine and Kubernetes pods"
   homepage "https://github.com/vishnudin/k8s-file-transfer"
-  version "1.0.0"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/vishnudin/k8s-file-transfer/releases/download/v1.0.0/Kubernetes%20File%20Transfer-1.0.0-mac.zip"
-      sha256 "247410f2bd8144c4f041678ff160506b2be5fd14ffbd4500e885337151ebd9c5"
+      url "https://github.com/vishnudin/k8s-file-transfer/releases/download/v1.0.2/Kubernetes.File.Transfer-1.0.2-mac.zip"
+      sha256 "0c4c07df3403daeb317add40dc41e28c8fc546ca2d09ec887d07831bed3eaf94"
     else
-      url "https://github.com/vishnudin/k8s-file-transfer/releases/download/v1.0.0/Kubernetes%20File%20Transfer-1.0.0-arm64-mac.zip"
-      sha256 "82193203c9252918f1cf26af05b390f1af35e5c66bb0e34b97f21b973e0078cc"
+      url "https://github.com/vishnudin/k8s-file-transfer/releases/download/v1.0.2/Kubernetes.File.Transfer-1.0.2-arm64-mac.zip"
+      sha256 "26fbf333a6f91b338a8410a231a1f20f61890fa8cf6878602229878898c3707c"
     end
   end
 
   depends_on "kubectl"
 
+  # Skip automatic linkage fixing for Electron apps
+  def self.needs_linkage_fixing?
+    false
+  end
+
   def install
     prefix.install "Kubernetes File Transfer.app"
-    bin.write_exec_script "#{prefix}/Kubernetes File Transfer.app/Contents/MacOS/Kubernetes File Transfer"
+
+    # Create a symlink in bin for command line access
+    (bin/"k8s-file-transfer").write <<~EOS
+      #!/bin/bash
+      exec "#{prefix}/Kubernetes File Transfer.app/Contents/MacOS/Kubernetes File Transfer" "$@"
+    EOS
   end
 
   def caveats
@@ -27,8 +37,14 @@ class K8sFileTransfer < Formula
       1. kubectl installed and configured with access to your Kubernetes clusters
       2. Proper permissions to list pods and execute kubectl cp commands
 
-      Launch the application from Applications folder or run:
+      The application has been installed to:
+        #{prefix}/Kubernetes File Transfer.app
+
+      You can launch it with:
         k8s-file-transfer
+
+      Or open it directly:
+        open "#{prefix}/Kubernetes File Transfer.app"
 
       For more information, visit: https://github.com/vishnudin/k8s-file-transfer
     EOS
@@ -36,5 +52,6 @@ class K8sFileTransfer < Formula
 
   test do
     assert_predicate prefix/"Kubernetes File Transfer.app", :exist?
+    assert_predicate bin/"k8s-file-transfer", :exist?
   end
 end
